4e22341aa75a9454a214aa01c973175a6d2931f6
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-10-20 22:40:38 +0000

    add a method that recent 5 most recent comments for a given post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   belongs_to :author, class_name: 'User'
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(3)
+  end
 end
