4031bec185c4c63d09932bdcbdb1e515c64e4f24
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-10-21 20:05:07 +0000

    clean code

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,10 @@ class Post < ApplicationRecord
   has_many :likes
   belongs_to :author, class_name: 'User'
 
-  validate :title, presence: true, length:{maximum: 250}
+  validate :title, presence: true, length: { maximum: 250 }
   validate :comments_counter, numericality: { greater_than_or_equal_to: 0 }
   validate :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
