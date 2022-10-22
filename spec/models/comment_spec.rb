3ba47bab5090f260daf059408730c3c3e71ba844
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-10-22 00:23:46 +0000

    add test for post

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   subject { Comment.new(text: 'This is a comment', author_id: 1, post_id: 1) }
   before { subject.save }
-    user = User.new(name: 'John', photo: 'https://usplash.com/photos/F_-0BxGuVvo', bio: 'I am a software developer', post_counter: 0)
-    post = Post.new(title: 'Post 1', text: 'Post 1 text', comments_counter: 0, likes_counter: 0, author_id: user)
+  user = User.new(name: 'John', photo: 'https://usplash.com/photos/F_-0BxGuVvo', bio: 'I am a software developer',
+                  post_counter: 0)
+  post = Post.new(title: 'Post 1', text: 'Post 1 text', comments_counter: 0, likes_counter: 0, author_id: user)
 
-  context 'after_save'  do
-
+  context 'after_save' do
     it 'should call update_comment_counter ' do
       expect(post.comments_counter).to_not eq(1)
     end
