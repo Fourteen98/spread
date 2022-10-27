be7e258ffc26a541bef126d1a57b13c9bb6dc81c
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-10-27 03:45:23 +0000

    add test for index page

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
+  context 'the index method' do
+    it 'returns http success on all users' do
+      get '/'
       expect(response).to have_http_status(:success)
     end
+    it "return rendered index page" do
+      get '/users'
+      expect(assigns(:users)).to render_template('index')
+    end
+    it ' shows all users' do
+      get users_path
+      expect(assigns(:users)).to eq(User.all)
+    end
+    it 'the view body is correct (index)' do
+      get('/users')
+      expect(response.body).to include('<div class="main">')
+    end
+  end
+
+  context 'the show method' do
+
   end
 end
+
+
+
