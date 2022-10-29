a712ffc068dff9e241f4682c9a8ed25b5f0478e6
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-10-29 19:40:32 +0000

    fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
 
   def show
     @user = User.includes(:posts, posts: [:comments, { comments: [:author] }]).find(params[:user_id])
-    @post = Post.includes(:comments, comments:[:author]).find(params[:id])
+    @post = Post.includes(:comments, comments: [:author]).find(params[:id])
     @comments = Comment.where(post_id: @post.id).order(created_at: :desc)
   end
 
@@ -23,17 +23,16 @@ class PostsController < ApplicationController
     pp params.inspect
 
     if @post.save!
-      puts " i got here !!"
+      puts ' i got here !!'
       redirect_to user_posts_path(current_user.id)
     else
       render :new
     end
   end
 
-
   private
+
   def post_params
     params.require(:post).permit(:title, :text)
   end
-
 end
