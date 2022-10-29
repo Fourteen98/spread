140b8e4052bc4d90a2a049eec7099934fbf84cca
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-10-29 16:29:56 +0000

    add forms and new and create controller for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,33 @@ class PostsController < ApplicationController
     @posts = Post.where(author_id: @user.id).order(created_at: :desc)
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+
+    pp params.inspect
+
+    if @post.save!
+      puts " i got here !!"
+      redirect_to user_posts_path(current_user.id)
+    else
+      render :new
+    end
+  end
+
   def show
     @post = Post.find(params[:id])
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
