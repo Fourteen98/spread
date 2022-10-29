0a6553275005abde47c4d82a2f736086c020ea70
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-10-29 19:37:16 +0000

    add likes forms

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,18 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    @comment.author_id = current_user.id
+    @comment.post_id = params[:post_id]
 
+    if @comment.save!
+      redirect_to user_post_path(params[:user_id], params[:post_id])
+    end
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
 end
