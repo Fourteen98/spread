39159ca7e8588e73d8be953cc7a8291b0f08f769
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-10-29 09:48:42 +0000

    finish views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.includes(:posts, posts: [:comments, {comments: [:author]}]).find(params[:user_id])
+    @posts = Post.where(author_id: @user.id).order(created_at: :desc)
   end
-
   def show
     @post = Post.find(params[:id])
   end
