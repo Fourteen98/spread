6eb69b1c254b9112f9e94fdc71a27b93b7d61eaf
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-02 15:01:31 +0000

    add test for user_show page

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature 'UserShows', type: :feature do
   describe 'Show', bullet: :skip do
     before(:example) do
-      @usr1 = User.create(id: 1, name: 'Abraha', photo: 'https://www.google.com/image/1', bio: 'From Ethiopia', posts_counter: 9)
+      @usr1 = User.create(id: 1, name: 'Abraha', photo: 'https://www.google.com/image/1', bio: 'From Ethiopia', post_counter: 9)
       5.times do |post|
         Post.create(title: "Post-#{post + 1}",
                     text: 'Capybara it is, did not know it was an animal',
@@ -20,7 +20,7 @@ RSpec.feature 'UserShows', type: :feature do
     end
 
      it 'Is displaying the number of posts the user has written.' do
-        expect(page).to have_content('Number of posts: 9')
+        expect(page).to have_content('Number of post: 5')
      end
 
     it 'It is displaying the users bio.' do
@@ -34,21 +34,18 @@ RSpec.feature 'UserShows', type: :feature do
     end
 
     it 'I can see the button that lets me view all of a users posts.' do
-      expect(page).to have_content('See All Posts')
+      expect(page).to have_content('See All Post')
     end
 
     it 'When I click a users post, it redirects me to that post show page.' do
+      click_link 'See All Post'
       click_link('Post-1')
-      expect(page).to have_content('Post-1')
+      expect(page.current_path).to eql('/users/1/posts/1')
     end
 
     it 'When I click the to see all post, it redirects me to the users post index page.' do
-      click_link('See All Posts')
-      expect(page).to have_content('Post-1')
-      expect(page).to have_content('Post-2')
-      expect(page).to have_content('Post-3')
-      expect(page).to have_content('Post-4')
-      expect(page).to have_content('Post-5')
+      click_link('See All Post')
+      expect(page.current_path).to eql('/users/1/posts')
     end
   end
 end
