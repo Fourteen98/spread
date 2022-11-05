c14e933d22cbdc199b5486c8c14918edbbffb62a
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-05 20:02:22 +0000

    fix linters

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::UsersController < Api::V1::ApplicationController
-
   def index
     @users = User.all
     render json: @users
@@ -9,5 +8,4 @@ class Api::V1::UsersController < Api::V1::ApplicationController
     @user = User.find(params[:id])
     render json: @user
   end
-
 end
